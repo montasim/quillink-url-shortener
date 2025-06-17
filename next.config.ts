0d9fbb4d6e54ff0416e8b240456d12013d48a7f3
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2025-06-17 15:07:08 +0600

    fix: google login

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     /* configuration options here */
-    env: {
-        NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-        NEXT_PUBLIC_GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
-    },
 };
 
 export default nextConfig;
