92a0ce80ccb804bfd744f13b2bc0f936d57dba7a
montasim <mmamun@mymedicalhub.com>
2025-07-22 17:01:33 +0600

    feat: add allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
         NEXT_PUBLIC_GOOGLE_REDIRECT_URI:
             process.env.GOOGLE_OAUTH_GOOGLE_REDIRECT_URI,
     },
+    allowedDevOrigins: ['192.168.0.106'],
 };
 
 export default nextConfig;
