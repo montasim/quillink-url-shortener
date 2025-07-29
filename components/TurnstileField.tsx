35f10780f9a9752b15041ce72f67a055ee488c37
montasim <mmamun@mymedicalhub.com>
2025-07-29 20:58:13 +0600

    fix: turnstile issue

--- components/TurnstileField.tsx
+++ components/TurnstileField.tsx
@@ -13,15 +13,19 @@ interface Props {
 }
 
 const TurnstileField = ({ onVerify }: Props) => {
+    const siteKey = process.env.NEXT_PUBLIC_CF_TURNSTILE_SITE_KEY;
+
+    if (!siteKey) {
+        return;
+    }
+
     return (
         <FormItem>
             <FormLabel>Verify</FormLabel>
             <FormControl>
                 <div className="w-full">
                     <Turnstile
-                        turnstileSiteKey={
-                            process.env.NEXT_PUBLIC_CF_TURNSTILE_SITE_KEY! || ''
-                        }
+                        turnstileSiteKey={siteKey}
                         callback={onVerify}
                         theme="light"
                     />
